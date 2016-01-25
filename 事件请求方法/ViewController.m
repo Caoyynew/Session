099f5198db0c6c5bf9290a64cc33a3b63e4caf1f
caoyangyang <841033655@qq.com>
2016-01-25 23:19:19 +0800

    1119

--- 事件请求方法/ViewController.m
+++ 事件请求方法/ViewController.m
@@ -8,20 +8,48 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<NSURLSessionDataDelegate>
+{
+    NSMutableData *recDates;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSURL *url = [NSURL URLWithString:@"http://115.159.1.248:56666/index.html"];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *task = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"%@",[[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
+        
+    }];
+    
+    NSURLSession *session1 = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:self delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task1 = [session1 dataTaskWithURL:url];
+    [task resume];
+    [task1 resume];
+}
+#pragma mark - NSURLSession datadelete
+-(void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler
+{
+    NSLog(@"开始！");
+    completionHandler(NSURLSessionResponseAllow);
+    recDates = [[NSMutableData alloc]init];
+}
+-(void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data
+{
+    NSLog(@"接受！");
+    [recDates appendData:data];//可能不是一次接受完成
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+-(void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error
+{
+    
+    NSLog(@"完成！recDatas=%@",[[NSString alloc]initWithData:recDates encoding:NSUTF8StringEncoding]);
+    
 }
+
 
 @end
